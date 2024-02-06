72d6b5b9410ce404410c6a4e469374e061b803e9
Denis2982 <denisdenis2982@gmail.com>
2024-02-06 15:32:15 +0200

    add not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,9 @@
 const NotFound = () => {
   return (
-    <div>not-found</div>
+    <div className="notFound__inner">
+      <h1 className="notFound__title">404</h1>
+      <p className="notFound__text">Page not found</p>
+    </div>
   )
 }
 
