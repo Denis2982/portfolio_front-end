3faa7b62abf831bae8c2b6857605bcfd3252b500
Denis2982 <denisdenis2982@gmail.com>
2024-02-06 12:29:22 +0200

    add loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,12 @@
+const Loading = () => {
+  return (
+    <div className="loadingInner">
+      <div className="loadingContainer">
+      <div className="loading"></div>
+      <div id="loadingText">loading</div>
+    </div>
+    </div>
+  );
+};
+
+export default Loading;
